483c142722381f367a72006dc30e9dbb7e817f65
Mazen Mohamed <132310334+Mazen-WebDev@users.noreply.github.com>
2023-05-15 04:05:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let angleUp = document.querySelector(".angleup");
 
+let box2 = document.querySelector(".box2");
+let span = document.querySelectorAll(".prog span");
+
+let nums = document.querySelectorAll(".fi-box .num");
+let smallBox = document.querySelector(".small-box");
+let start = false;
+
 window.onscroll = function () {
     // if (this.scrollY >= 1000) {
     //     angleUp.classList.add("show");
@@ -7,6 +14,18 @@ window.onscroll = function () {
     //     angleUp.classList.remove("show");
     // }
     this.scrollY >= 1000 ? angleUp.classList.add("show") : angleUp.classList.remove("show");
+    if (window.scrollY >= smallBox.offsetTop - 450) {
+        if (!start) {
+            nums.forEach((num) => startCount(num));
+        }
+        start = true;
+    }
+
+    if (window.scrollY >= box2.offsetTop - 400) {
+        span.forEach((span) => {
+            span.style.width = span.dataset.width;
+        });
+    }
 }
 
 angleUp.onclick = function () {
@@ -14,4 +33,14 @@ angleUp.onclick = function () {
         top: 0,
         behavior: "smooth"
     });
+}
+
+function startCount(e) {
+    let goal = e.dataset.goal;
+    let count = setInterval(() => {
+        e.textContent++;
+        if (e.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
 }
